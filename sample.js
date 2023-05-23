de88f7b55a5e57565933c83a8e9d655d969807f2
kanoloa <46836245+kanoloa@users.noreply.github.com>
2023-05-23 11:21:44 +0900

    Update sample.js
    #6213 change the calcuration logic.

--- sample.js
+++ sample.js
@@ -2,9 +2,10 @@
 # do not use this script in production code.
 
 let cb = 2210;
+let factor = 2;
 
 for (let i = 0; i < cb; i++) {
-  logger.debug("counter = " + i);
+  logger.debug("counter = " + i + ", powered = " + (i * factor));
 }
 
 ### end of javascript code.
