9883cd96889b06526283bfbc0193d54746ae88a5
kanoloa <46836245+kanoloa@users.noreply.github.com>
2023-07-27 10:25:14 +0900

    Update sample.js
    #7564 Modified for initial settings.

--- sample.js
+++ sample.js
@@ -1,10 +1,8 @@
 # modify by kanoloa, 23 May 2023
 # do not use this script in production code.
 # addtional lise.
-# more comment
-# 今日は暑いですね。
 
-let cb = 2210;
+let cb = 2212;
 let factor = 2.13;
 
 for (let i = 0; i < cb; i++) {
