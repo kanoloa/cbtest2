6d40584d7615ee3e4ef8e2fc5292d5e13b7f63c9
kanoloa <46836245+kanoloa@users.noreply.github.com>
2023-08-15 10:13:05 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,9 +6,11 @@ let cb = 2212;
 let factor = 2.14;
 let version = "22.10 sp3";
 
-for (let i = 0; i < cb; i++) {
-  logger.debug("counter = " + i + ", powered = " + (i * factor));
+function sample() {
+  for (let i = 0; i < cb; i++) {
+    logger.debug("counter = " + i + ", powered = " + (i * factor));
+  }
+  logger.debug("this code was executed using " + version);
 }
-logger.debug("this code was executed using " + version);
 
 ### end of javascript code.
